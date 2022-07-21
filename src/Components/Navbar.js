a47a46ea6f95776da66c6ab7f14378a1ac52e46c
sanjay12345 <58308698+JUGALMEHRA958@users.noreply.github.com>
2022-07-21 23:39:33 +0530

    ini

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,17 +12,21 @@ function MyNavbar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+            <Nav.Link href="#link">Shop</Nav.Link>
+            <Nav.Link href="#link">Store collections</Nav.Link>
+            <Nav.Link href="#link">About us</Nav.Link>
+            <Nav.Link href="#link">Contact us</Nav.Link>
+            <Nav.Link href="#link">Our specialities</Nav.Link>
+            <NavDropdown title="Username" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                Edit profile
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Cart</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Your orders
+              </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
